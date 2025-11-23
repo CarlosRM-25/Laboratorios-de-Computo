4e704aac97de74c75a56b15e902ab5c84814d5a1
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-11-23 14:13:34 -0600

    Add files via upload

---
+++ Controllers/ReservasController1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Gestión_de_Reservas.Data;
+using Gestión_de_Reservas.Models;
+
+namespace Gestión_de_Reservas.Controllers
+{
+    public class ReservasController1 : Controller
+    {
+        // GET: ReservasController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ReservasController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservasController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservasController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservasController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservasController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservasController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservasController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
